e15112d8ba26f0795d69bdda4122a6d92a667b13
jordiklavers <154262445+jordiklavers@users.noreply.github.com>
2025-04-09 15:30:06 +0200

    swipers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-initNavDropdownMobile();
+$("document").ready(function() {
+  initNavDropdownMobile();
+  initSwipers();
+});
 
 function initNavDropdownMobile() {
   const navDropdown = $(".nav_dropdown");
@@ -18,5 +21,50 @@ function initNavDropdownMobile() {
     });
   }
 }
-
 $(window).on('resize', initNavDropdownMobile);
+
+
+function initSwipers() {
+  const dienstenSwiper = new Swiper(".swiper.is-diensten", {
+    navigation: {
+      nextEl: "[data-diensten-swiper='next']", 
+      prevEl: "[data-diensten-swiper='prev']"
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  })
+  const reviewsSwiper = new Swiper(".swiper.is-reviews", {
+    centeredSlides: true,
+    loop: true, // Laat de slides continu herhalen
+    autoplay: {
+      delay: 3000, // Automatisch afspelen met een vertraging van 3 seconden
+      disableOnInteraction: false, // Automatisch afspelen hervatten na interactie
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 24,
+      },
+    },
+  });
+}
